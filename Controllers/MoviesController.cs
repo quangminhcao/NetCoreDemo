b4c9be14e19d3efb07ef32d7ad24e309d775bdda
unknown <lokiqwe1231@gmail.com>
2021-11-24 14:30:48 +0700

    add search

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,19 +20,35 @@ namespace NetCoreDemo.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string SearchString)
-        {
-            //select danh sach cac ban ghi movie trong database
-            var movieslist = from m in _context.Movie
-                 select m;
-            if (!string.IsNullOrEmpty(SearchString))
-            {
-                movieslist = movieslist.Where(m => m.Title.Contains(SearchString));
-            }
-            //tra ve list movie voi dieu kien Title co chua tu khoa tim kiem (bat dong bo)
-            return View(await movieslist.ToListAsync());
-        }
+        // GET: Movies
+public async Task<IActionResult> Index(string movieGenre, string searchString)
+{
+    // Use LINQ to get list of genres.
+    IQueryable<string> genreQuery = from m in _context.Movie
+                                    orderby m.Genre
+                                    select m.Genre;
 
+    var movies = from m in _context.Movie
+                 select m;
+
+    if (!string.IsNullOrEmpty(searchString))
+    {
+        movies = movies.Where(s => s.Title.Contains(searchString));
+    }
+
+    if (!string.IsNullOrEmpty(movieGenre))
+    {
+        movies = movies.Where(x => x.Genre == movieGenre);
+    }
+
+    var movieGenreVM = new MovieGenreViewModel
+    {
+        Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+        Movies = await movies.ToListAsync()
+    };
+
+    return View(movieGenreVM);
+}
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
         {
